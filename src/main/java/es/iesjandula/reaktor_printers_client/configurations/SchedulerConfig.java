47df25bad4bb8fb089b96ddad5bb112523a6fdf8
pakkk <paco.benitez.chico@gmail.com>
2024-09-24 12:53:12 +0200

    Añadido scheduler de 10 thread por yaml

---
+++ src/main/java/es/iesjandula/reaktor_printers_client/configurations/SchedulerConfig.java
@@ -0,0 +1,24 @@
+package es.iesjandula.reaktor_printers_client.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig
+{
+	@Value("${reaktor.threadsNumber}")
+	private Integer threadsNumber ;
+	
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler()
+    {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler() ;
+        
+        taskScheduler.setPoolSize(this.threadsNumber) ;
+        taskScheduler.setThreadNamePrefix("ScheduledTask-") ;
+        
+        return taskScheduler ;
+    }
+}
